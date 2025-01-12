72ed370ad7d959219b687b97db3468f3b49b69db
dbstream <bergstrom.db@proton.me>
2025-01-12 17:52:19 +0100

    syscalls: document syscall ABI on x86
    The purpose of 'unstable' syscalls is to be used temporarily while
    developing kernel interfaces.  No 'real' application should use them.

--- arch/x86/include/asm/syscall_nr.h
+++ arch/x86/include/asm/syscall_nr.h
@@ -7,6 +7,12 @@
 #ifndef __DAVIX_ASM_SYSCALL_NR_H
 #define __DAVIX_ASM_SYSCALL_NR_H 1
 
+#define __SYS_STABLE_MIN		0
+#define __SYS_STABLE_MAX		1023
+
+#define __SYS_UNSTABLE_MIN		1048576
+#define __SYS_UNSTABLE_MAX		1049599
+
 #define __SYS_enosys			0
 #define __SYS_write_dmesg		1
 #define __SYS_exit			2
