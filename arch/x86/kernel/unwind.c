8167afd8bbb08504076015f63b22d5e7e37b72f2
dbstream <bergstrom.db@proton.me>
2025-02-19 23:01:51 +0100

    x86/unwind: implement address->symbol lookup

--- arch/x86/kernel/unwind.c
+++ arch/x86/kernel/unwind.c
@@ -9,15 +9,59 @@
 #include <asm/sections.h>
 #include <asm/unwind.h>
 
+static inline const char *
+get_embedded_name (unsigned long offset)
+{
+	return (const char *) &__kernel_end[offset];
+}
+
 static inline unsigned long
-readat (unsigned long offset)
+read_embed_data (unsigned long offset)
 {
 	return *(unsigned long *) &__kernel_end[offset];
+}
+
+struct symbol_data {
+	const char *name;
+	unsigned long start;
+	unsigned long size;
+};
+
+static struct symbol_data
+find_symbol (unsigned long addr)
+{
+	unsigned long a, b, m, o, start, size;
+
+	o = read_embed_data (8);
+
+	a = 0;
+	b = read_embed_data (16) - 1;
+	while (a != b) {
+		m = (a + b + 1) / 2;
+		if (read_embed_data (o + 24 * m) > addr)
+			b = m - 1;
+		else
+			a = m;
+	}
+
+	start = read_embed_data (o + 24 * a + 0);
+	size = read_embed_data (o + 24 * a + 8);
+	const char *name = get_embedded_name (read_embed_data (o + 24 * a + 16));
+
+	if (addr < start || (start + size && addr >= start + size)) {
+		start = 0;
+		size = 0;
+		name = "?";
+	}
+
+	return (struct symbol_data) { name, start, size };
 }
 
 void
 dump_backtrace (void)
 {
-	printk ("embed data: addr=0x%lx  data[0]=0x%lx  data[1]=0x%lx\n",
-			(unsigned long) &__kernel_end[0], readat (0), readat (8));
+	unsigned long addr = (unsigned long) &dump_backtrace + 10;
+	struct symbol_data sym = find_symbol (addr);
+	printk ("symbol lookup test:  0x%lx  (%s + 0x%lx)\n",
+			addr, sym.name, addr - sym.start);
 }
--- arch/x86/tools/gen_embed.cc
+++ arch/x86/tools/gen_embed.cc
@@ -6,19 +6,41 @@
  * file.  This is used for kernel access to the kernel symbols and for stack
  * unwind information.
  */
- #include <elf.h>
+#include <endian.h>
 #include <fcntl.h>
+#include <gelf.h>
+#include <libelf.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
+#include <algorithm>
+#include <string>
 #include <vector>
 
-static void *kernel;
+static Elf *kernel;
 
 static std::vector<uint8_t> output;
+
+static inline uint64_t
+add_cstring (const char *s)
+{
+	uint64_t offset = output.size ();
+	do {
+		output.push_back (*s);
+	} while (*(s++));
+	return offset;
+}
+
+static inline uint64_t
+align_to_8 (void)
+{
+	for (size_t i = output.size(); i & 7; i++)
+		output.push_back (0);
+	return output.size ();
+}
 
 static inline uint64_t
 add_zeroes (uint64_t n)
@@ -48,11 +70,25 @@ add_output (uint64_t x)
 	return set_u64 (add_zeroes (8), x);
 }
 
+struct Symbol {
+	uint64_t value;
+	uint64_t size;
+	uint64_t name_offset;
+	std::string name;
+};
+
+static std::vector<Symbol> symbols;
+
 int
 main (int argc, char **argv)
 {
 	if (argc != 2) {
 		fprintf (stderr, "usage:  gen_embed path/to/davix.elf > output\n");
+		return 1;
+	}
+
+	if (elf_version (EV_CURRENT) != EV_CURRENT) {
+		fprintf (stderr, "failed to initialize libelf: %s\n", elf_errmsg (-1));
 		return 1;
 	}
 
@@ -62,35 +98,84 @@ main (int argc, char **argv)
 		return 1;
 	}
 
+	kernel = elf_begin (fd, ELF_C_READ, NULL);
+	if (!kernel) {
+		fprintf (stderr, "elf_begin() failed: %s\n", elf_errmsg(-1));
+		return 1;
+	}
+
+	if (elf_kind (kernel) != ELF_K_ELF) {
+		fprintf (stderr, "elf_kind() is not ELF_K_ELF\n");
+		return 1;
+	}
+
 	/**
-	 * Perform some basic validations to make sure that a kernel ELF binary
-	 * is (probably) provided to the tool.
+	 * embed[0...7] = sizeof embed
+	 * embed[8...15] = symbol offset
+	 * embed[16...23] = symbol count
 	 */
+	add_zeroes (24);
 
-	struct stat stat;
-	if (fstat (fd, &stat) == -1) {
-		perror ("failed to stat kernel ELF binary");
+	Elf_Scn *scn = NULL;
+	GElf_Shdr shdr;
+	while ((scn = elf_nextscn (kernel, scn))) {
+		if (gelf_getshdr (scn, &shdr) != &shdr) {
+			fprintf (stderr, "gelf_getshdr() failed: %s\n", elf_errmsg (-1));
+			return 1;
+		}
+
+		if (shdr.sh_type == SHT_SYMTAB)
+			break;
+	}
+
+	if (!scn) {
+		fprintf (stderr, "couldn't find SHT_SYMTAB in kernel ELF binary\n");
 		return 1;
 	}
 
-	if (stat.st_size < sizeof (Elf64_Ehdr)) {
-		fprintf (stderr, "kernel binary is too small\n");
+	Elf_Data *data = elf_getdata (scn, NULL);
+	if (!data) {
+		fprintf (stderr, "elf_getdata() failed: %s\n", elf_errmsg (-1));
 		return 1;
 	}
 
-	kernel = mmap (NULL, stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-	if (kernel == MAP_FAILED) {
-		perror ("failed to mmap kernel ELF binary");
-		return 1;
+	const auto count = shdr.sh_size / shdr.sh_entsize;
+	for (auto i = 0; i < count; i++) {
+		GElf_Sym sym;
+		if (gelf_getsym (data, i, &sym) != &sym) {
+			fprintf (stderr, "gelf_getsym() failed: %s\n", elf_errmsg (-1));
+			return 1;
+		}
+
+		unsigned char type = ELF64_ST_TYPE (sym.st_info);
+		if (type != STT_FUNC)
+			continue;
+
+		const char *p = elf_strptr (kernel, shdr.sh_link, sym.st_name);
+		if (!p) {
+			fprintf (stderr, "elf_strptr() failed: %s\n", elf_errmsg (-1));
+			return 1;
+		}
+
+		Symbol s = { sym.st_value, sym.st_size, 0, p };
+		symbols.push_back (std::move (s));
 	}
 
-	if (memcmp (kernel, "\x7F\x45\x4c\x46", 4) != 0) {
-		fprintf (stderr, "kernel binary does not appear to be an ELF binary\n");
-		return 1;
+	std::sort (std::begin (symbols), std::end (symbols), [](const Symbol &lhs, const Symbol &rhs){
+		return lhs.value < rhs.value;
+	});
+
+	for (Symbol &sym : symbols)
+		sym.name_offset = add_cstring (sym.name.c_str ());
+
+	set_u64 (8, align_to_8 ());
+	set_u64 (16, symbols.size ());
+	for (Symbol &sym : symbols) {
+		add_output (sym.value);
+		add_output (sym.size);
+		add_output (sym.name_offset);
 	}
 
-	add_zeroes (8);
-	add_output (42);
 	set_u64 (0, output.size ());
 	if (write (STDOUT_FILENO, output.data (), output.size()) != output.size ()) {
 		fprintf (stderr, "failed to write output\n");
