d4c31f4af6fad35ed9f3e3fe36f379db1a57344d
dbstream <bergstrom.db@proton.me>
2025-02-23 17:59:15 +0100

    x86/asm: force mmio_* helpers to use %rax

--- arch/x86/include/asm/mmio.h
+++ arch/x86/include/asm/mmio.h
@@ -11,7 +11,7 @@ static inline uint8_t
 mmio_read8 (const void *p)
 {
 	uint8_t x;
-	asm volatile ("movb (%1), %0" : "=r" (x) : "r" (p) : "memory");
+	asm volatile ("movb (%1), %0" : "=a" (x) : "r" (p) : "memory");
 	return x;
 }
 
@@ -19,7 +19,7 @@ static inline uint16_t
 mmio_read16 (const void *p)
 {
 	uint16_t x;
-	asm volatile ("movw (%1), %0" : "=r" (x) : "r" (p) : "memory");
+	asm volatile ("movw (%1), %0" : "=a" (x) : "r" (p) : "memory");
 	return x;
 }
 
@@ -27,7 +27,7 @@ static inline uint32_t
 mmio_read32 (const void *p)
 {
 	uint32_t x;
-	asm volatile ("movl (%1), %0" : "=r" (x) : "r" (p) : "memory");
+	asm volatile ("movl (%1), %0" : "=a" (x) : "r" (p) : "memory");
 	return x;
 }
 
@@ -35,32 +35,32 @@ static inline uint64_t
 mmio_read64 (const void *p)
 {
 	uint64_t x;
-	asm volatile ("movq (%1), %0" : "=r" (x) : "r" (p) : "memory");
+	asm volatile ("movq (%1), %0" : "=a" (x) : "r" (p) : "memory");
 	return x;
 }
 
 static inline void
 mmio_write8 (void *p, uint8_t x)
 {
-	asm volatile ("movb %0, (%1)" :: "r" (x), "r" (p) : "memory");
+	asm volatile ("movb %0, (%1)" :: "a" (x), "r" (p) : "memory");
 }
 
 static inline void
 mmio_write16 (void *p, uint16_t x)
 {
-	asm volatile ("movw %0, (%1)" :: "r" (x), "r" (p) : "memory");
+	asm volatile ("movw %0, (%1)" :: "a" (x), "r" (p) : "memory");
 }
 
 static inline void
 mmio_write32 (void *p, uint32_t x)
 {
-	asm volatile ("movl %0, (%1)" :: "r" (x), "r" (p) : "memory");
+	asm volatile ("movl %0, (%1)" :: "a" (x), "r" (p) : "memory");
 }
 
 static inline void
 mmio_write64 (void *p, uint64_t x)
 {
-	asm volatile ("movq %0, (%1)" :: "r" (x), "r" (p) : "memory");
+	asm volatile ("movq %0, (%1)" :: "a" (x), "r" (p) : "memory");
 }
 
 #endif /* _ASM_MMIO_H */
