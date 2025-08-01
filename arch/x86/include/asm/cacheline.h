744bba375f18f7d3a68132a13150d76844876de9
dbstream <bergstrom.db@proton.me>
2025-08-01 19:38:08 +0200

    asm: add cacheline.h header file

---
+++ arch/x86/include/asm/cacheline.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+/*
+ * File: include/asm/cacheline.h
+ * Cache properties and cache-related definitions.
+ */
+#pragma once
+
+/*
+ * A common cacheline size on recent x86_64 processors is 64 bytes.
+ */
+#define CACHELINE_SIZE 64
+
