ae915c0e9f21456ff45e3dc9e1e435d583121868
dbstream <bergstrom.db@proton.me>
2025-08-01 19:42:42 +0200

    Ke: implement KeRawSpinlock
    KeRawSpinlock is the "raw" spinlock type: it doesn't disable
    preemption/DPCs/IRQs itself when used, instead it will be wrapped by
    dedicated KeSpinlock/KeDPCSpinlock/KeIRQSpinlock structures that call
    into the KeRawSpinlock::lock, KeRawSpinlock::unlock functions.

---
+++ Ke/spinlock.cc
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: GPL-3.0
+/*
+ * File: Ke/spinlock.cc
+ * KeRawSpinlock slowpaths.
+ *
+ * Copyright (C) 2025  dbstream
+ */
+#define SPINLOCK_DEBUG 1
+
+#ifndef SPINLOCK_DEBUG
+#define SPINLOCK_DEBUG 0
+#endif
+
+#include <Ke/raw_spinlock.h>
+#include <davix/atomic.h>
+
+#if SPINLOCK_DEBUG
+#include <Ke/context.h>
+#include <davix/bug.h>
+#endif
+
+/**
+ * KeRawSpinlock::lock_slowpath - spinlock acquire slowpath.
+ * @lockval: last seen value in KeRawSpinlock::m_value
+ */
+void KeRawSpinlock::lock_slowpath(unsigned int lockval)
+{
+#if SPINLOCK_DEBUG
+	/*
+	 * It is a bug to acquire a spinlock in a preemptible context.
+	 *
+	 * NOTE: it is enough to disable one of {preemption, DPCs, IRQs} to
+	 * effectively prevent preemption.  If all of them are enabled, invoke
+	 * BUG().
+	 */
+	BUG_ON(KePreemptionEnabled() && KeDPCsEnabled() && KeIRQsEnabled());
+#endif
+
+	/*
+	 * This is a simple TTAS lock.
+	 */
+
+	for (;;) {
+		while (lockval == 1) {
+			if (atomic_cmpxchg_weak_for_lock(&m_value, &lockval, 0))
+				return;
+		}
+
+		do {
+			smp_spinwait_hint();
+			lockval = atomic_load_relaxed(&m_value);
+		} while (lockval == 0U);
+	}
+}
+
---
+++ include/Ke/raw_spinlock.h
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: GPL-3.0
+/*
+ * File: include/Ke/raw_spinlock.h
+ * KeRawSpinlock
+ *
+ * Copyright (C) 2025  dbstream
+ */
+#pragma once
+
+#include <davix/atomic.h>
+
+struct KeRawSpinlock {
+	unsigned int m_value = 1;
+
+	void lock_slowpath(unsigned int value);
+
+	inline void init(void)
+	{
+		m_value = 1;
+	}
+
+	/**
+	 * KeRawSpinlock::trylock - try to acquire the lock without spinning.
+	 */
+	inline bool trylock(void)
+	{
+		unsigned int exp = 1;
+		/*
+		 * NOTE: on some architectures, non-weak cmpxchg is implemented
+		 * as a ll/sc loop, which arguably counts as spinning.
+		 */
+		return atomic_cmpxchg_for_lock(&m_value, &exp, 0);
+	}
+
+	/**
+	 * KeRawSpinlock::lock - spin and acquire the lock.
+	 */
+	inline void lock(void)
+	{
+		unsigned int exp = 1;
+		if (!atomic_cmpxchg_weak_for_lock(&m_value, &exp, 0)) {
+			[[unlikely]];
+			lock_slowpath(exp);
+		}
+	}
+
+	/**
+	 * KeRawSpinlock::unlock - release the lock.
+	 */
+	inline void unlock(void)
+	{
+		atomic_store_release(&m_value, 1);
+	}
+};
+
