955b8da1f8770feda0ea03880da3bdb1dddfb4af
dbstream <bergstrom.db@proton.me>
2025-08-01 20:53:28 +0200

    Ke: add KeSpinlock, KeDPCSpinlock, KeIRQSpinlock types
    KeSpinlock is the most lightweight of these spinlock types. It only
    disables preemption before taking the lock. KeSpinlock should be
    preferred over the other spinlocks whenever possible, as it doesn't
    defer DPCs or IRQs.
    
    KeDPCSpinlock is a good middle-ground between KeSpinlock and
    KeIRQSpinlock, masking DPCs when it is held.
    
    KeIRQSpinlock is the heaviest spinlock that we provide. It should be
    used when synchronization is needed for interrupt contexts.

---
+++ include/Ke/spinlock.h
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: GPL-3.0
+/*
+ * File: include/Ke/spinlock.h
+ * Wrapper types for KeRawSpinlock.
+ *
+ * Copyright (C) 2025  dbstream
+ */
+#pragma once
+
+#include <Ke/context.h>
+#include <Ke/raw_spinlock.h>
+
+class KeSpinlock {
+	KeRawSpinlock m_lock;
+public:
+	inline void init(void)
+	{
+		m_lock.init();
+	}
+
+	inline bool trylock(void)
+	{
+		KeDisablePreemption();
+		if (m_lock.trylock())
+			return true;
+		KeEnablePreemption();
+		return false;
+	}
+
+	inline void lock(void)
+	{
+		KeDisablePreemption();
+		m_lock.lock();
+	}
+
+	inline void unlock(void)
+	{
+		m_lock.unlock();
+		KeEnablePreemption();
+	}
+};
+
+class KeDPCSpinlock {
+	KeRawSpinlock m_lock;
+public:
+	inline void init(void)
+	{
+		m_lock.init();
+	}
+
+	inline bool trylock(void)
+	{
+		KeDisableDPCs();
+		if (m_lock.trylock())
+			return true;
+		KeEnableDPCs();
+		return false;
+	}
+
+	inline void lock(void)
+	{
+		KeDisableDPCs();
+		m_lock.lock();
+	}
+
+	inline void unlock(void)
+	{
+		m_lock.unlock();
+		KeEnableDPCs();
+	}
+};
+
+class KeIRQSpinlock {
+	KeRawSpinlock m_lock;
+public:
+	inline void init(void)
+	{
+		m_lock.init();
+	}
+
+	inline bool trylock(void)
+	{
+		KeDisableIRQs();
+		if (m_lock.trylock())
+			return true;
+		KeEnableIRQs();
+		return false;
+	}
+
+	inline void lock(void)
+	{
+		KeDisableIRQs();
+		m_lock.lock();
+	}
+
+	inline void unlock(void)
+	{
+		m_lock.unlock();
+		KeEnableIRQs();
+	}
+};
+
